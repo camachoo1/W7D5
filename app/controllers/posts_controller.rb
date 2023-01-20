81cf7c287b086c60f5fcc66e81740b5e0506172b
“Omar <ocamacho812@gmail.com>
2023-01-20 15:25:42 -0800

    Started updating Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,6 +45,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :url, :content)
+    params.require(:post).permit(:title, :url, :content, :sub_ids)
   end
 end
